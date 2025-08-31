5451e6aad2247355f24e3d5a47b4e478aa901f42
Deepak <206972401+thedeepak12@users.noreply.github.com>
2025-08-31 18:42:25 +0530

    Add 'New Message' form

--- app.js
+++ app.js
@@ -9,18 +9,7 @@ app.set('views', './views');
 
 app.use(express.urlencoded({ extended: true }));
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+const messages = [];
 
 app.get('/', (req, res) => {
   res.render('index', {
@@ -28,6 +17,22 @@ app.get('/', (req, res) => {
     messages: messages
   });
 });
+
+app.get('/new', (req, res) => {
+  res.render('form', {
+    title: 'New Message',
+  });
+});
+
+app.post('/new', (req, res) => {
+  const { messageUser, messageText } = req.body;
+  messages.unshift({
+    text: messageText,
+    user: messageUser,
+    added: new Date()
+  });
+  res.redirect('/');
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
